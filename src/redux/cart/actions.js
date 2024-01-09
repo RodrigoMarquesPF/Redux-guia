58debe9680660511880d577b7b1efe0111b76f42
Danmok01 <rodrigomarquespf@gmail.com>
2024-01-09 02:21:09 -0300

    teste

---
+++ src/redux/cart/actions.js
@@ -0,0 +1,21 @@
+import CartActionTypes from "./action-types"
+
+export const addProductToCart = (payload) => ({
+    type: CartActionTypes.ADD_PRODUCT,
+    payload,
+});
+
+export const removeProductFromCart = (payload) => ({
+    type: CartActionTypes.REMOVE_PRODUCT,
+    payload,
+});
+
+export const increaseProduct = (payload) => ({
+    type: CartActionTypes.INCREASE_PRODUCT,
+    payload,
+});
+
+export const decreaseProduct = (payload) => ({
+    type: CartActionTypes.DECREASE_PRODUCT,
+    payload,
+});
